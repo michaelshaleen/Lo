c138bdc61e888f46d76f3509e26dce3665f93dfc
michaelshaleen <shaleenmichael@yahoo.com>
2021-10-05 17:18:57 -0500

    landing page spacing skeleton laid out

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -3,14 +3,23 @@ import React from 'react'
 
 import './Landing.css';
 function Landing() {
-  return (
+  return (<>
     <div>
       <p className="welcome">Welcome</p>
-      <p className="intro">Chances are you've had professional help before.
+      <p className="intro">
+        Chances are you've had professional help before.
         Yet here you are.
+        A key difference we often miss out on
+        is the fact that any improvements in our
+        health and fitness MUST first be lead by
+        improvements in our LIFESTYLE.
       </p>
     </div>
-  )
+
+    <div className="filler"></div>
+
+    <div className="more_content"></div>
+ </> )
 }
 
 export default Landing
